49a8108eeaaad37904bf44759fe4cd19c031332d
Kacprr <117747063+Kacprr@users.noreply.github.com>
2025-06-04 10:04:55 +0200

    added Dash and Enum playerstate
    added Well functioning Dash / Roll :
    locks player direction, can be cancelled by jumping, plays full animation
    Changed animation management to use an enum playerState and current state for consistancy and modularity

--- ruinsofnorth/Scripts/player.gd
+++ ruinsofnorth/Scripts/player.gd
@@ -1,8 +1,17 @@
 extends CharacterBody2D
 
-
-const SPEED = 130.0
+var SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+
+#enumeration used to track the current state, makes it easier to play animations
+enum PlayerState {
+	IDLE,
+	JUMP,
+	DASH,
+	RUN
+}
+
+var current_State
 
 var gravitiy = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -10,37 +19,58 @@ var gravitiy = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
+	if !is_on_floor():
+		current_State = PlayerState.JUMP
+		SPEED = 130
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction: -1, 0 ,1
 	var direction := Input.get_axis("move_left", "move_right")
-	
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		current_State = PlayerState.JUMP
+		velocity.y = JUMP_VELOCITY
+
+	#Handle dash
+	if Input.is_action_just_pressed("dash") and is_on_floor() and current_State != PlayerState.DASH and direction:
+		current_State = PlayerState.DASH
+		var active_direction = direction
+		$DashTimer.start()
+		direction = active_direction
+		SPEED *= 1.5
+		velocity.x = direction * SPEED
+
 	# Flip the Sprite
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
 	
-	# Play Animations
+	# Play Animations based on the current_state
 	if is_on_floor():
-		if direction == 0:
+		if current_State == PlayerState.IDLE:
 			animated_sprite.play("idle")
-		else:
+		elif current_State == PlayerState.DASH:
+			animated_sprite.play("dash")
+		elif current_State == PlayerState.RUN:
 			animated_sprite.play("run")
-	else:
+	elif current_State == PlayerState.JUMP: #still a little scuffed
 		animated_sprite.play("jump")
 	
-	
-	
 	# Apply Movement
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if current_State != PlayerState.DASH:
+		if direction:
+			if is_on_floor():
+				current_State = PlayerState.RUN
+			velocity.x = direction * SPEED
+		else:
+			current_State = PlayerState.IDLE
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+#After dash is complete reseting speed back to base speed and ending dash
+func _on_dash_timer_timeout() -> void:
+	current_State = PlayerState.IDLE
+	SPEED = 130.0
