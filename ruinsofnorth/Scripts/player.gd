9648d662adff4e766f2019fd76c685064a92d8c2
Berent <berent2002@hotmail.com>
2025-10-21 14:39:26 +0200

    Dash problem fix, sfx added, debug_enemy deleted.
    The dash animation not being occuring and the bug that dashing speed going up to infinite has been fixed.
    
    The bug that dash animation merges with jump has been fixed (total reorganize for animation handling block).
    
    The sfx of ; jump,dash,hurt has been added.
    
    Debug_enemy has been removed since its no important anymore.

--- ruinsofnorth/Scripts/player.gd
+++ ruinsofnorth/Scripts/player.gd
@@ -2,11 +2,10 @@ extends CharacterBody2D
 
 var SPEED = 130.0
 const JUMP_VELOCITY = -300.0
-var health = 5
+var health = 10
 var is_invulnerable = false
 var jumps_left: int = 1 # Start with 1 extra jump (for a total of 2)
 
-#enumeration used to track the current state, makes it easier to play animations
 enum PlayerState {
 	IDLE,
 	JUMP,
@@ -22,21 +21,27 @@ signal health_changed(health)
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var attack_scene = preload("res://scenes/Attack.tscn")
 
+@onready var jump_sound: AudioStreamPlayer2D = $JumpSound
+@onready var dash_sound: AudioStreamPlayer2D = $DashSound
+@onready var hurt_sound: AudioStreamPlayer2D = $HurtSound
+
 
 func _ready():
 	add_to_group("Player")
 
 
 func _physics_process(_delta: float) -> void:
+	const DASH_SPEED = 200.0
 	# Add the gravity.
 	if !is_on_floor():
-		current_State = PlayerState.JUMP
-		SPEED = 130
+		if current_State != PlayerState.DASH:
+			current_State = PlayerState.JUMP
 		velocity.y += gravity * _delta
 	# --- JUMP/DOUBLE JUMP LOGIC ---
 	if is_on_floor():
 		jumps_left = 1
-		current_State = PlayerState.IDLE #Resets state when landing.
+		if current_State != PlayerState.DASH:
+			current_State = PlayerState.IDLE #Resets state when landing.
 
 
 	# Get the input direction: -1, 0 ,1
@@ -46,12 +51,14 @@ func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		current_State = PlayerState.JUMP
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
 		
 	# Handle double-jump
 	elif Input.is_action_just_pressed("jump") and jumps_left > 0:
 		current_State = PlayerState.JUMP
 		velocity.y = JUMP_VELOCITY
-		jumps_left -= 1 #Decrement the counter!
+		jumps_left -= 1  #Decrement the counter!
+		jump_sound.play()
 	
 
 	#Handle dash
@@ -60,17 +67,17 @@ func _physics_process(_delta: float) -> void:
 		var active_direction = direction
 		$DashTimer.start()
 		direction = active_direction
-		SPEED *= 1.5
-		velocity.x = direction * SPEED
+		velocity.x = direction * DASH_SPEED
+		dash_sound.play()
 		
+	if current_State != PlayerState.DASH:
 		# Flip the Sprite
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
+		if direction > 0:
+			animated_sprite.flip_h = false
+		elif direction < 0:
+			animated_sprite.flip_h = true
 	
 		# Apply Movement
-	if current_State != PlayerState.DASH:
 		if direction:
 			if is_on_floor():
 				current_State = PlayerState.RUN
@@ -83,16 +90,15 @@ func _physics_process(_delta: float) -> void:
 		
 
 	# Play Animations based on the current_state
-	if is_on_floor():
+	if current_State == PlayerState.DASH:
+		animated_sprite.play("dash")
+	elif !is_on_floor():
+		animated_sprite.play("jump")
+	elif is_on_floor():
 		if current_State == PlayerState.IDLE:
 			animated_sprite.play("idle")
-		elif current_State == PlayerState.DASH:
-			animated_sprite.play("dash")
 		elif current_State == PlayerState.RUN:
 			animated_sprite.play("run")
-	elif current_State == PlayerState.JUMP:
-		animated_sprite.play("jump")
-	
 
 func _unhandled_input(event):
 	if event.is_action_pressed("attack"):
@@ -113,7 +119,6 @@ func spawn_attack():
 #After dash is complete reseting speed back to base speed and ending dash
 func _on_dash_timer_timeout() -> void:
 	current_State = PlayerState.IDLE
-	SPEED = 130.0
 
 func die():
 	print("You died!")
@@ -127,6 +132,8 @@ func take_damage(amount, knockback = Vector2.ZERO):
 	health -= amount
 	emit_signal("health_changed", health)
 	print("Player took damage. Health: ", health)
+	
+	hurt_sound.play()
 
 	is_invulnerable = true
 	modulate = Color(1, 0.5, 0.5) # Red tint
