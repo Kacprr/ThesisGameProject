486bf0387b149ebe069f5bcb512355e6075d52b5
Berent <berent2002@hotmail.com>
2025-10-30 19:23:37 +0100

    last fix
    last fix before merging;
    hitbox hides longer after attacking

--- ruinsofnorth/Scripts/attack.gd
+++ ruinsofnorth/Scripts/attack.gd
@@ -2,11 +2,12 @@ extends Area2D
 
 @export var damage: int = 1
 @onready var _animated_sprite = $AnimatedSprite2D
+@onready var collision_shape_2d: CollisionPolygon2D = $CollisionShape2D
 
 var _recent_hits := {}
 var _hit_cooldown := 0.2
 
-const KNOCKBACK_POWER: float = 300
+const KNOCKBACK_POWER: float = 150
 const KNOCKBACK_UP_MULTIPLIER: float = 0.5
 
 func _ready():
@@ -23,13 +24,20 @@ func _on_body_entered(body: Node2D) -> void:
 		return
 	if body.is_in_group("enemies"):
 		if body.has_method("take_damage"):
+			_recent_hits[body] = true
+			
 			var knockback_direction: Vector2 = (body.global_position - global_position).normalized()
 			knockback_direction.y -= KNOCKBACK_UP_MULTIPLIER
 			
 			var knockback_vector: Vector2 = knockback_direction.normalized() * KNOCKBACK_POWER
 			body.take_damage(damage, knockback_vector)
 			
-			_recent_hits[body] = true
+			if collision_shape_2d:
+				collision_shape_2d.set_deferred("disabled", true)
+				await get_tree().create_timer(0.5).timeout
+			if collision_shape_2d:
+				collision_shape_2d.set_deferred("disabled", false)
+			
 			# clear recent hit after short delay to avoid multi-hit in one overlap
 			await get_tree().create_timer(_hit_cooldown).timeout
 			_recent_hits.erase(body)
