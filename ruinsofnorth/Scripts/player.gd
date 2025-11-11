ceafc425d85f59a36eb315ec263cdf5cf41a8ead
Kacprr <kacperhetman4@gmail.com>
2025-11-11 18:01:15 +0100

    walljump now costs 10 stamina
    ^^^
    stamina cost to be determined

--- ruinsofnorth/Scripts/player.gd
+++ ruinsofnorth/Scripts/player.gd
@@ -113,6 +113,7 @@ func _physics_process(_delta: float) -> void:
 			if (Input.is_action_just_pressed("jump")):
 				velocity.y = JUMP_VELOCITY
 				current_State = PlayerState.JUMP
+				current_stamina -= 10
 				jump_sound.play()
 			animated_sprite.flip_h = (direction_x < 0)
 		else:
