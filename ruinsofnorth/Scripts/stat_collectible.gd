24e9e6dc2914893b07a6e1f3bca6f601aa421cfa
Berent <berent2002@hotmail.com>
2025-11-14 17:19:28 +0100

    Stat Collectible Update
    - stat collectible upgrade script been updated to be eligible with new upcoming chest feat.

--- ruinsofnorth/Scripts/stat_collectible.gd
+++ ruinsofnorth/Scripts/stat_collectible.gd
@@ -19,39 +19,47 @@ enum UpgradeType { HEALTH, STAMINA }
 @onready var audio_player: AudioStreamPlayer2D = $AudioPlayer
 
 func _ready():
-	# 1. Hide the visual elements until the choice is made
+	# 1. Hide the visual elements
 	health_area.visible = false
 	stamina_area.visible = false
-	
+
+func activate_loot():
 	var player = get_tree().get_first_node_in_group("Player")
 	
-	if player:
-		process_upgrade_choice(player)
-	queue_free()
+	if not is_instance_valid(player):
+		queue_free()
+		return
+	process_upgrade_choice(player)
 
 func process_upgrade_choice(player: CharacterBody2D):
 	var bonus = randi_range(min_bonus, max_bonus)
 	
 	var random_choice = randi() % 2
 	var type = UpgradeType.HEALTH if random_choice == 0 else UpgradeType.STAMINA
+	var sprite_to_show: AnimatedSprite2D = health_sprite
 	
 	if type == UpgradeType.HEALTH:
 		if player.has_method("upgrade_max_health"):
 			player.upgrade_max_health(bonus)
-			play_pickup_effect(health_sprite)
+			sprite_to_show = health_sprite
 			
 	elif type == UpgradeType.STAMINA:
 		if player.has_method("upgrade_max_stamina"):
 			player.upgrade_max_stamina(bonus)
-			play_pickup_effect(stamina_sprite)
+			sprite_to_show = stamina_sprite
 
-func play_pickup_effect(sprite_to_show: AnimatedSprite2D):
+	play_pickup_effect(sprite_to_show)
+
+func play_pickup_effect(sprite_to_show: AnimatedSprite2D): # Tried to do effect here!
 	sprite_to_show.visible = true
+	# Disabling collisions before the effect begins
+	health_collision.set_deferred("disabled", true)
+	stamina_collision.set_deferred("disabled", true)
+	
 	audio_player.stream = pickup_sound
 	audio_player.play()
 	
 	await get_tree().create_timer(0.3).timeout
 	sprite_to_show.visible = false
 	await audio_player.finished
-	
-	pass
+	queue_free()
