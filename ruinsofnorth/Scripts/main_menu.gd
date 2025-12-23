ee6a24b410c0f72339ccde24e00e753499e1580c
Berent <berent2002@hotmail.com>
2025-12-23 23:04:11 +0300

    Pause Menu Bug Fix, Options Menu Improved
    - Options menu layout have been improved with the new keybinding options. (Still need some allignment and visual improvements)
    
    - Pause menu bug making the game restarts after entering options menu via pause menu has been fixed.

--- ruinsofnorth/Scripts/main_menu.gd
+++ ruinsofnorth/Scripts/main_menu.gd
@@ -5,7 +5,6 @@ func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/game.tscn") #main game scene
 
 func _on_options_button_pressed():
-	print("Opening options...")
 	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
 
 func _on_exit_button_pressed():
