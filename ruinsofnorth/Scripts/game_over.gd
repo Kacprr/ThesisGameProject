b7adabcb66858fb1686221fd17c65c7f68116d3f
Kacprr <kacperhetman4@gmail.com>
2026-01-16 18:28:49 +0100

    Civan fixes
    Game_over scene node name changes
    text changes
    respawn font change

--- ruinsofnorth/Scripts/game_over.gd
+++ ruinsofnorth/Scripts/game_over.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
-@onready var play_again_button: Button = $VBoxContainer/PlayAgainButton
+@onready var new_game_button: Button = $VBoxContainer/NewGame
 @onready var quit_button: Button = $VBoxContainer/QuitButton
 
 const GAME_SCENE = "res://Scenes/game.tscn"
@@ -10,14 +10,12 @@ func _ready():
 	get_tree().paused = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
-	play_again_button.text = "New Game"
-
-	
 	# Respawn Logic
 	if Globals.checkpoint_active and Globals.score >= 3:
 		var respawn_btn = Button.new()
 		respawn_btn.text = "Respawn (3 Coins)"
 		respawn_btn.add_theme_font_size_override("font_size", 24)
+		respawn_btn.add_theme_font_override("font", load("res://Assets/fonts/PixelOperator8.ttf") )
 		
 		# Insert it before the Quit button (at index 1, since Play Again is 0)
 		$VBoxContainer.add_child(respawn_btn)
@@ -25,7 +23,7 @@ func _ready():
 		
 		respawn_btn.pressed.connect(_on_respawn_button_pressed)
 
-func _on_play_again_button_pressed():
+func _on_new_game_pressed() -> void:
 	Globals.reset_checkpoint()
 	Globals.reset_stats() # Optional: also reset coins/health/etc for a fresh start
 	Globals.respawning = false
@@ -36,6 +34,7 @@ func _on_play_again_button_pressed():
 	queue_free()
 	
 	get_tree().change_scene_to_file(GAME_SCENE)
+
 
 func _on_quit_button_pressed():
 	get_tree().paused = false
